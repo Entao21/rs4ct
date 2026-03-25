4706b004b9f4682d2dec3868602112791f067e61
Entao Wang <entao.wang.21@ucl.ac.uk>
2026-03-25 00:14:57 +0000

    1

--- practicals/week2.R
+++
@@ -1,8 +0,0 @@
-remotes::install_github("yihui/xaringan")
-install.packages("xaringanExtra")
-library(xaringan)
-library(xaringanExtra)
-
-packageVersion("xaringan")
-packageVersion("xaringanExtra")
-
